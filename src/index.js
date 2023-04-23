43ade8beae35240b9c535824f5c09d27dc26365b
jahnavi3333 <110617871+jahnavi3333@users.noreply.github.com>
2023-04-23 19:44:23 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import List from './List';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <List></List>
   </React.StrictMode>
 );
 
